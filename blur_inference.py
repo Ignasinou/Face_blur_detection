ee3cba39d6c5b9c44c214154a4f835cb02bc8207
ignasinou@gmail.com <ignasinou@gmail.com>
2022-08-08 18:39:02 +0200

    Inference video

---
+++ blur_inference.py
@@ -0,0 +1,249 @@
+import cv2
+from image_blur_score import BlurScore
+import argparse
+import tqdm
+from centerface import CenterFace
+import onnxruntime as ort
+import subprocess
+
+
+def get_faces_from_centerface(frame, centerface_threshold, centerface):
+    h, w = frame.shape[:2]
+    width = w
+    r = width / float(w)
+    height = int(h * r)
+
+    x_scale = w / width
+    y_scale = h / height
+
+    dets, lms = centerface(frame, height, width, threshold=centerface_threshold)
+
+    faces = []
+    boxes = []
+
+    for ff_idx, det in enumerate(dets):
+        box, score = det[:4], det[4]
+        x1 = int(box[0] * x_scale)
+        y1 = int(box[1] * y_scale)
+        x2 = int(box[2] * x_scale)
+        y2 = int(box[3] * y_scale)
+
+        face = frame[y1: y2, x1: x2, :]
+        boxes.append([x1, y1, x2, y2])
+        faces.append(face)
+
+    return faces, boxes
+
+
+def main():
+
+
+    sess_options = ort.SessionOptions()
+    centerface = CenterFace(ort, sess_options, landmarks=True)
+    parser = argparse.ArgumentParser(description="Face_blur_detection")
+    parser.add_argument('--videoFile')
+    parser.add_argument('--centerface_threshold', type=float, default=0.7, help="centerface th")
+
+    parser.add_argument('--th_Brenner', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 5383350')
+    parser.add_argument('--th_Laplacian', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 66658734')
+    parser.add_argument('--th_Thenengrad', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 4.71')
+    parser.add_argument('--th_SMD', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 6.44')
+    parser.add_argument('--th_SMD2', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 1982.758')
+    parser.add_argument('--th_Variance', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 15.73')
+    parser.add_argument('--th_Energy', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 2650.78')
+    parser.add_argument('--th_Vollath', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 6592.09')
+    parser.add_argument('--th_Entropy', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 6521070215.58')
+    parser.add_argument('--th_JPEG', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 11.924')
+    parser.add_argument('--th_JPEG2', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 4.999561272269467')
+    parser.add_argument('--th_Gaussian_Laplacian', type=float, default=0.5, help='If provisional is 0 please use the following value as reference: 12.60')
+
+    parser.add_argument('--provisional_th_base', type=int, default=0, choices=[0, 1])
+
+    args = parser.parse_args()
+
+    blur_score = BlurScore()
+
+    file_extension = "." + args.videoFile.split(".")[-1]
+    output_video_filename = args.videoFile.replace(file_extension, '_output.mp4')
+    output_audio_filename = args.videoFile.replace(file_extension, '.wav')
+
+    video = cv2.VideoCapture(args.videoFile)
+    num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    # th is the not normalized th base (blur)
+    th_Brenner_base = 5383350
+    th_Laplacian_base = 66658734
+    th_Thenengrad_base = 4.71
+    th_SMD_base = 6.44
+    th_SMD2_base = 1982.758
+    th_Variance_base = 15.73
+    th_Energy_base = 2650.78
+    th_Vollath_base = 6592.09
+    th_Entropy_base = 6521070215.58
+    th_JPEG_base = 11.924
+    th_JPEG2_base = 4.999561272269467
+    th_Gaussian_Laplacian_base = 12.60
+
+    if args.provisional_th_base:
+        th_Brenner = args.th_Brenner * (th_Brenner_base * 2)
+        th_Laplacian = args.th_Laplacian * (th_Laplacian_base * 2)
+        th_Thenengrad = args.th_Thenengrad * (th_Thenengrad_base * 2)
+        th_SMD = args.th_SMD * (th_SMD_base * 2)
+        th_SMD2 = args.th_SMD2 * (th_SMD2_base * 2)
+        th_Variance = args.th_Variance * (th_Variance_base * 2)
+        th_Energy = args.th_Energy * (th_Energy_base * 2)
+        th_Vollath = args.th_Vollath * (th_Vollath_base * 2)
+        th_Entropy = args.th_Entropy * (th_Entropy_base * 2)
+        th_JPEG = args.th_JPEG * (th_JPEG_base * 2)
+        th_JPEG2 = args.th_JPEG2 * (th_JPEG2_base * 2)
+        th_Gaussian_Laplacian = args.th_Gaussian_Laplacian * (th_Gaussian_Laplacian_base * 2)
+
+    else:
+        th_Brenner = args.th_Brenner
+        th_Laplacian = args.th_Laplacian
+        th_Thenengrad = args.th_Thenengrad
+        th_SMD = args.th_SMD
+        th_SMD2 = args.th_SMD2
+        th_Variance = args.th_Variance
+        th_Energy = args.th_Energy
+        th_Vollath = args.th_Vollath
+        th_Entropy = args.th_Entropy
+        th_JPEG = args.th_JPEG
+        th_JPEG2 = args.th_JPEG2
+        th_Gaussian_Laplacian = args.th_Gaussian_Laplacian
+
+    nested_blur_dict = {
+        "Brenner": {'th': th_Brenner},
+        "Laplacian": {'th': th_Laplacian},
+        "Thenengrad": {'th': th_Thenengrad},
+        "SMD": {'th': th_SMD},
+        "SMD2": {'th': th_SMD2},
+        "Variance": {'th': th_Variance},
+        "Energy": {'th': th_Energy},
+        "Vollath": {'th': th_Vollath},
+        "Entropy": {'th': th_Entropy},
+        "JPEG": {'th': th_JPEG},
+        "JPEG2": {'th': th_JPEG2},
+        "Gaussian_Laplacian": {'th': th_Gaussian_Laplacian}}
+
+    for key, value in nested_blur_dict.items():
+        output_video = output_video_filename.replace('_output.mp4', f'_{key}_output.mp4')
+        value['video'] = cv2.VideoWriter(
+            output_video
+            , cv2.VideoWriter_fourcc(*'XVID'), fps, (width, height))
+
+    while True:
+        for frame_num in tqdm.tqdm(range(int(num_frames))):
+            ret, frame = video.read()
+
+            if not ret:
+                break
+            faces, boxes = get_faces_from_centerface(frame,
+                                                     centerface_threshold=args.centerface_threshold,
+                                                     centerface=centerface)
+
+            for face, bbox in zip(faces, boxes):
+
+                #--------- _Brenner ---------
+                score_Brenner = blur_score._Brenner(face)
+
+                # --------- _Laplacian ---------
+                score_Laplacian = blur_score._Laplacian(face)
+
+                # --------- _Thenengrad ---------
+                score_Thenengrad = blur_score._Thenengrad(face)
+
+                # --------- _SMD ---------
+                score_SMD = blur_score._SMD(face)
+
+                # --------- _SMD2 ---------
+                score_SMD2 = blur_score._SMD2(face)
+
+                # --------- _Variance ---------
+                score_Variance = blur_score._Variance(face)
+
+                # --------- _Energy ---------
+                score_Energy = blur_score._Energy(face)
+
+                # --------- _Vollath ---------
+                score_Vollath = blur_score._Vollath(face)
+
+                # --------- _Entropy ---------
+                score_Entropy = blur_score._Entropy(face)
+
+                # --------- _JPEG ---------
+                score_JPEG = blur_score._JPEG(face)
+
+                # --------- _JPEG2 ---------
+                score_JPEG2 = blur_score._JPEG2(face)
+
+                # --------- _Gaussian_Laplacian ---------
+                score_Gaussian_Laplacian = blur_score._Gaussian_Laplacian(face)
+
+                nested_blur_dict['Brenner']['last_score'] = score_Brenner
+                nested_blur_dict['Laplacian']['last_score'] = score_Laplacian
+                nested_blur_dict['Thenengrad']['last_score'] = score_Thenengrad
+                nested_blur_dict['SMD']['last_score'] = score_SMD
+                nested_blur_dict['SMD2']['last_score'] = score_SMD2
+                nested_blur_dict['Variance']['last_score'] = score_Variance
+                nested_blur_dict['Energy']['last_score'] = score_Energy
+                nested_blur_dict['Vollath']['last_score'] = score_Vollath
+                nested_blur_dict['Entropy']['last_score'] = score_Entropy
+                nested_blur_dict['JPEG']['last_score'] = score_JPEG
+                nested_blur_dict['JPEG2']['last_score'] = score_JPEG2
+                nested_blur_dict['Gaussian_Laplacian']['last_score'] = score_Gaussian_Laplacian
+
+                for key, value in nested_blur_dict.items():
+
+                    frame_copy = frame.copy()
+
+                    outVideo = value['video']
+
+                    if not len(faces):
+                        outVideo.write(frame_copy)
+                        continue
+
+                    last_blur_score = float(value['last_score'])
+                    blur_th = float(value['th'])
+
+                    if last_blur_score >= blur_th:
+                        color = (0, 255, 0)
+                    else:
+                        color = (0, 0, 255)
+
+                    last_blur_score = float(f"{last_blur_score:0.5f}")
+                    frame_copy = cv2.rectangle(frame_copy, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 10)
+                    cv2.putText(frame_copy, f"{last_blur_score}", (bbox[0], bbox[1] + 30), cv2.FONT_HERSHEY_SIMPLEX,
+                                1, (255, 255, 255), 2, cv2.LINE_AA)
+                    cv2.putText(frame_copy, f"BLUR METHOD: {key}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                                1, (0, 255, 0), 2, cv2.LINE_AA)
+                    value['last_frame'] = frame_copy.copy()
+
+                for key, value in nested_blur_dict.items():
+                    outVideo = value['video']
+                    outVideo.write(value['last_frame'])
+
+            if frame_num >= 300:
+                break
+
+        break
+
+    for key, value in nested_blur_dict.items():
+        outVideo = value['video']
+        outVideo.release()
+
+    cv2.destroyAllWindows()
+    video.release()
+
+    # subprocess.call(
+    #     'ffmpeg -i %s -i %s -shortest  %s -y' % (output_video_filename,
+    #                                              output_audio_filename,
+    #                                              output_video_filename.replace(file_extension, "_AV.mp4")),
+    #     shell=True)
+
+
+if __name__ == '__main__':
+    main()
--- image_blur_score.py
+++ image_blur_score.py
@@ -5,21 +5,17 @@
 import os
 import cv2
 import numpy as np
-import mtcnn.tools_matrix as tools
-from mtcnn.test import get_bbox
-from skimage.transform import estimate_transform, warp
 from skimage import filters
 import math
 
 class BlurScore:
-    def __init__(self, strDir):
+    def __init__(self):
         """
         创建BlurScore对象
         :param: strDir 存放测试图像的文件夹路径
         :return:  BlurScore对象
         """
         print("BlurScore object is created...")
-        self.strDir = strDir # strDir：保存测试图像的文件夹路径
 
     def _getAllImg(self):
         """
@@ -61,15 +57,15 @@ class BlurScore:
         return cropped_image
 
 
-    def _preprocess(self, imgName, do_gray = True):
+    def _preprocess(self, img, do_gray = True):
         """
         图像的读取及预处理操作
         :param imgName: 图像的名称
         :return: 经过剪裁人脸及灰度化之后的图片对象
         """
         # 剪裁处理
-        cropImg = self._cropImgByMtcnn(imgName)
-        
+        # cropImg = self._cropImgByMtcnn(imgName)
+        cropImg = img
         # 将图片压缩为单通道的灰度图（可选）
         if do_gray:
             img2gray = cv2.cvtColor(cropImg.astype('float32'), cv2.COLOR_BGR2GRAY)
@@ -111,14 +107,13 @@ class BlurScore:
         return 
 
 
-    def _Brenner(self, imgName):
+    def _Brenner(self, face):
         """
         指标一：Brenner梯度函数
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
-        img2gray, cropImg = self._preprocess(imgName, do_gray = True)
-        #cv2.imwrite(r"/home/mmlab2/Face_blur_detection/"+str(imgName)+"_crop.jpg",cropImg,[int(cv2.IMWRITE_JPEG_QUALITY),70])
+        img2gray, cropImg = self._preprocess(face, do_gray = True)
         f = self._imageToMatrix(img2gray)
         x, y = f.shape
         score = 0
@@ -128,24 +123,24 @@ class BlurScore:
         return score
 
 
-    def _Laplacian(self, imgName):
+    def _Laplacian(self, face):
         """
         指标二：拉普拉斯方差算法
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
-        cropImg = self._preprocess(imgName, do_gray = False)
+        cropImg = self._preprocess(face, do_gray = False)
         score = cv2.Laplacian(cropImg, cv2.CV_64F).var()
         return score
 
 
-    def _Thenengrad(self, imgName):
+    def _Thenengrad(self, face):
         """
         指标三：Thenengrad梯度函数
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
-        img2gray, cropImg = self._preprocess(imgName, do_gray = True)
+        img2gray, cropImg = self._preprocess(face, do_gray = True)
         f = self._imageToMatrix(img2gray)
 
         tmp = filters.sobel(f)
@@ -154,14 +149,14 @@ class BlurScore:
         return score
     
 
-    def _SMD(self, imgName):
+    def _SMD(self, face):
         """
         指标四：SMD灰度方差函数
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
         # 图像的预处理
-        img2gray, cropImg = self._preprocess(imgName, do_gray = True)
+        img2gray, cropImg = self._preprocess(face, do_gray = True)
         f = self._imageToMatrix(img2gray)/255.0
         x, y = f.shape
         score = 0
@@ -172,14 +167,14 @@ class BlurScore:
         return score
         
         
-    def _SMD2(self, imgName):
+    def _SMD2(self, face):
         """
         指标五：SMD2灰度方差函数
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
         # 图像的预处理
-        img2gray, cropImg = self._preprocess(imgName, do_gray = True)
+        img2gray, cropImg = self._preprocess(face, do_gray = True)
         f = self._imageToMatrix(img2gray)/255.0
         x, y = f.shape
         score = 0
@@ -190,27 +185,27 @@ class BlurScore:
         return score
         
     
-    def _Variance(self, imgName):
+    def _Variance(self, face):
         """
         指标六：方差函数
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
         # 图像的预处理
-        img2gray, cropImg = self._preprocess(imgName, do_gray = True)
+        img2gray, cropImg = self._preprocess(face, do_gray = True)
         f = self._imageToMatrix(img2gray)
         score = np.var(f)
         
         return score
         
         
-    def _Energy(self, imgName):
+    def _Energy(self, face):
         """
         指标七：能量梯度函数
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
-        img2gray, cropImg = self._preprocess(imgName, do_gray = True)
+        img2gray, cropImg = self._preprocess(face, do_gray = True)
         f = self._imageToMatrix(img2gray)
         x, y = f.shape
         score = 0
@@ -221,13 +216,13 @@ class BlurScore:
         return score
     
     
-    def _Vollath(self, imgName):
+    def _Vollath(self, face):
         """
         指标八：Vollath函数
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
-        img2gray, cropImg = self._preprocess(imgName, do_gray = True)
+        img2gray, cropImg = self._preprocess(face, do_gray = True)
         f = self._imageToMatrix(img2gray)
         score = 0
         x, y = f.shape
@@ -239,13 +234,13 @@ class BlurScore:
         return score
         
     
-    def _Entropy(self, imgName):
+    def _Entropy(self, face):
         """
         指标九：熵函数
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
-        img2gray, cropImg = self._preprocess(imgName, do_gray = True)
+        img2gray, cropImg = self._preprocess(face, do_gray = True)
         f = np.array(img2gray,dtype = 'int64')
         x, y = f.shape
         count = x*y
@@ -258,13 +253,13 @@ class BlurScore:
         return score
         
     
-    def _JPEG(self, imgName):
+    def _JPEG(self, face):
         """
         指标十：论文No-Reference Perceptual Quality Assessment of JPEG Compressed Images
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
-        img2gray, cropImg = self._preprocess(imgName, do_gray = True)
+        img2gray, cropImg = self._preprocess(face, do_gray = True)
         f = np.array(img2gray)
         x,y = f.shape
         # 水平方向
@@ -338,18 +333,18 @@ class BlurScore:
         return S
         
         
-    def _JPEG2(self, imgName):
+    def _JPEG2(self, face):
         """
         指标十一：论文No-Reference Image Quality Assessment forJPEG/JPEG2000 Coding
         :param imgName: 图像的名称
         :return: 模糊度分数
         """
-        S = self._JPEG(imgName)
+        S = self._JPEG(face)
         SS = 1 + 4 / (1 + math.exp((-1.0217) * (S-3)))
     
         return SS
     
-    def _Gaussian_Laplacian(self, imgName):
+    def _Gaussian_Laplacian(self, face):
         '''
         指标十二：
         对采集到的人脸图像进行如下处理：
@@ -359,17 +354,16 @@ class BlurScore:
         4.直方图归一化映射到0-255，
         5.求均值方差，方差的阈值为300
         '''
-        strPath = os.path.join(self.strDir, imgName)
-        img = cv2.imread(strPath)
-        #img = self._preprocess(imgName, do_gray = False)
+
+
         # 高斯滤波
-        gauss_blur = cv2.GaussianBlur(img,(3,3),0)
+        gauss_blur = cv2.GaussianBlur(face, (3, 3), 0)
         # 使用线性变换转换输入数组元素成8位无符号整型 归一化为0-255
         transform = cv2.convertScaleAbs(gauss_blur)
         # 灰度化
         grey = cv2.cvtColor(transform, cv2.COLOR_BGR2GRAY)
         # 使用3x3的Laplacian算子卷积滤波
-        grey_laplace = cv2.Laplacian(grey, cv2.CV_16S,ksize=3)
+        grey_laplace = cv2.Laplacian(grey, cv2.CV_16S, ksize=3)
         # 归一化为0-255
         resultImg = cv2.convertScaleAbs(grey_laplace)
         
@@ -380,5 +374,5 @@ class BlurScore:
         return blurPer
         
 if __name__ == "__main__":
-    BlurScore = BlurScore(strDir=r"/home/mmlab2/Face_blur_detection/isblur")
+    BlurScore = BlurScore()
     BlurScore.getAllScore() 
